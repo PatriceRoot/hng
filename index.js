ea07e5810f79d51579007bed07ae708f6b28e82a
PatriceRoot <patricekalwira@gmail.com>
2024-07-04 14:03:10 +0200

    job done

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
+import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import requestIp from 'request-ip';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-const PORT = process.env;
+const port = process.env.PORT || 3000;
 
-app.get("/api/hello", async (req, res) => {
+app.use(requestIp.mw());
+
+app.get('/api/hello', async (req, res) => {
   const visitorName = req.query.visitor_name;
-  const clientIp = req.ip;
+  const clientIp = req.clientIp;
 
   try {
-    // Utiliser une API de géolocalisation pour obtenir la ville (par exemple, ipapi)
-    const locationResponse = await axios.get(
-      `http://ip-api.com/json/${clientIp}`
-    );
+    const locationResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
     const location = locationResponse.data.city;
 
-    // Utiliser une API météo pour obtenir la température (par exemple, OpenWeatherMap)
-    const weatherResponse = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric`
-    );
+    const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric`);
     const temperature = weatherResponse.data.main.temp;
 
-    // Créer la réponse
     const response = {
       client_ip: clientIp,
       location,
-      greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`,
+      greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
     };
 
     res.json(response);
   } catch (error) {
-    res.status(500).send("Error retrieving data");
+    console.error('Error retrieving data:', error);
+    res.status(500).send('Error retrieving data');
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running {PORT}!`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
